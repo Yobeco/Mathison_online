77c4d987f45a7f5a958c1ac66ddf41ab9bc6ff6b
Yobeco <ybecognee@yahoo.fr>
2024-05-10 19:57:05 -0600

    Arrow keys for keyboardless play. Works on touch screen ?

--- script.js
+++ script.js
@@ -1,59 +1,61 @@
 //      Mathison
-//  Objectif : Ajouter clavier virtuel pour fonctionner sur téléphone
+//  Objectif : Ajouter du pavet numérique et de la barre espace
 // 
 
-const lecteurAudio = document.getElementById('lecteurAudio');
-const lecteurSon = document.getElementById('lecteurSon');
+document.addEventListener('DOMContentLoaded', function() {
 
+    const lecteurAudio = document.getElementById('lecteurAudio');
+    const lecteurSon = document.getElementById('lecteurSon');
+    const boutonStop = document.getElementById('stopButton');
 
-function stopAudio() {
-    lecteurAudio.pause(); // Arrête la lecture
-    lecteurAudio.currentTime = 0; // Rembobine au début
-    // Changer la source de l'image
-    document.getElementById('picture').src = 'Medias/LogoMathison.png';
-    document.getElementById('pictureContainer').focus();
-}
+    boutonStop.onclick = function() {
+      stopAudio();
+    };
 
+    function stopAudio() {
+        lecteurAudio.pause(); // Arrête la lecture
+        lecteurAudio.currentTime = 0; // Rembobine au début
+        // Changer la source de l'image
+        document.getElementById('picture').src = 'Medias/LogoMathison.png';
+        document.getElementById('pictureContainer').focus();
+    }
 
-function leaveFocus(){
-    // Empêcher l'événement par défaut (le focus du lecteur audio)
-    event.preventDefault();
-    // Déplacez le focus vers le conteneur d'images
-    document.getElementById('pictureContainer').focus();
-    lecteurAudio.blur();
-    console.log("Focus vers image");
-}
+    function leaveFocus(){
+        // Empêcher l'événement par défaut (le focus du lecteur audio)
+        event.preventDefault();
+        // Déplacez le focus vers le conteneur d'images
+        document.getElementById('pictureContainer').focus();
+        lecteurAudio.blur();
+        console.log("Focus vers image");
+    }
 
-// Dès que le lecteur prend le focus, il perd le focus : la priorité est que l'image est le focus
-lecteurAudio.addEventListener('focus', function() {
-    leaveFocus();
-});
+    // Dès que le lecteur prend le focus, il perd le focus : la priorité est que l'image est le focus
+    lecteurAudio.addEventListener('focus', function() {
+        leaveFocus();
+    });
 
-// Variable "flag" pour le changement de la couleur des flèches
-let color = true
+    // Variable "flag" pour le changement de la couleur des flèches
+    let color = true
 
-// À étudier pour simplifier le code :
-// let dictKey = {
-//   "ArrowUp" : [104,"Up-Y","Up-B"],
-//   "ArrowDown" : [98,"Down-Y","Down-B"]
-// };
+    // À étudier pour simplifier le code :
+    // let dictKey = {
+    //   "ArrowUp" : [104,"Up-Y","Up-B"],
+    //   "ArrowDown" : [98,"Down-Y","Down-B"]
+    // };
 
-/**
-Codes pour les touches du pavé numérique :
-    NumLock : 144
-    / (Division) : 111
-    * (Multiplication) : 106
-    - (Soustraction) : 109
-    + (Addition) : 107
-    Enter (Entrée du pavé numérique) : 108 ou 13 (varie selon les navigateurs)
-    . (Point décimal) : 110
-    0 à 9 : 96 à 105
-**/
+    /**
+    Codes pour les touches du pavé numérique :
+        NumLock : 144
+        / (Division) : 111
+        * (Multiplication) : 106
+        - (Soustraction) : 109
+        + (Addition) : 107
+        Enter (Entrée du pavé numérique) : 108 ou 13 (varie selon les navigateurs)
+        . (Point décimal) : 110
+        0 à 9 : 96 à 105
+    **/
 
-document.addEventListener('keydown', function(event) {
-
-    // Si flèche du haut appuyée ou  8 du pavet numérique:
-    if (event.key === 'ArrowUp' || event.keyCode === 104) {
+    function ArrowUpFunc(){
         lecteurSon.play();
 
         // Changer la source de l'image
@@ -67,8 +69,7 @@ document.addEventListener('keydown', function(event) {
         color = !color
     }
 
-
-    if (event.key === 'ArrowDown' || event.keyCode === 98) {
+    function ArrowDownFunc(){
         lecteurSon.play();
 
         // Changer la source de l'image
@@ -82,7 +83,7 @@ document.addEventListener('keydown', function(event) {
         color = !color
     }
 
-    if (event.key === 'ArrowLeft' || event.keyCode === 102) {
+    function ArrowLeftFunc(){
         lecteurSon.play();
 
         // Changer la source de l'image
@@ -95,29 +96,77 @@ document.addEventListener('keydown', function(event) {
         color = !color
     }
 
-    if (event.key === 'ArrowRight' || event.keyCode === 100) {
-        lecteurSon.play();
+    function ArrowRightFunc(){
+            lecteurSon.play();
 
-        // Changer la source de l'image
-        if (color == true){
-            document.getElementById('picture').src = 'Medias/Right-Y.png';
-        }
-        else {
-            document.getElementById('picture').src = 'Medias/Right-B.png';
-        }
-        color = !color
+            // Changer la source de l'image
+            if (color == true){
+                document.getElementById('picture').src = 'Medias/Right-Y.png';
+            }
+            else {
+                document.getElementById('picture').src = 'Medias/Right-B.png';
+            }
+            color = !color
     }
 
 
-    if (event.code === 'Space' || event.keyCode === 32) {
 
-        if (lecteurAudio.paused) {
-          lecteurAudio.play(); // Arrête la lecture
-        } else if (lecteurAudio.play) {
-          lecteurAudio.pause(); // Arrête la lecture
+    document.addEventListener('keydown', function(event) {
+
+        // Si flèche du haut appuyée ou  8 du pavet numérique:
+        if (event.key === 'ArrowUp' || event.keyCode === 104) {
+            ArrowUpFunc();
         }
 
-    }
+        if (event.key === 'ArrowDown' || event.keyCode === 98) {
+            ArrowDownFunc();
+        }
 
+        if (event.key === 'ArrowLeft' || event.keyCode === 102) {
+            ArrowLeftFunc();
+        }
+
+        if (event.key === 'ArrowRight' || event.keyCode === 100) {
+            ArrowRightFunc();
+        }
+
+
+        if (event.code === 'Space' || event.keyCode === 32) {
+
+            if (lecteurAudio.paused) {
+              lecteurAudio.play(); // Arrête la lecture
+            } else if (lecteurAudio.play) {
+              lecteurAudio.pause(); // Arrête la lecture
+            }
+
+        }
+
+
+    });
+
+
+    const KeyPadUp = document.getElementById('keyPadUp')
+
+    KeyPadUp.addEventListener('click', function() {
+        ArrowUpFunc();
+    });
+
+    const KeyPadDown = document.getElementById('keyPadDown')
+
+    KeyPadDown.addEventListener('click', function() {
+        ArrowDownFunc();
+    });
+
+    const KeyPadLeft = document.getElementById('keyPadLeft')
+
+    KeyPadLeft.addEventListener('click', function() {
+        ArrowLeftFunc();
+    });
+
+    const KeyPadRight = document.getElementById('keyPadRight')
+
+    KeyPadRight.addEventListener('click', function() {
+        ArrowRightFunc();
+    });
 
 });
